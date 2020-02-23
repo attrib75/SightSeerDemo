c6b874e65520666b6d1791cf1b54bc18b8fa3333
pgoode <pgoode75@sbcglobal.net>
2020-02-23 00:30:41 -0600

    Add project files.

---
+++ SightSeerDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SightSeerDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
